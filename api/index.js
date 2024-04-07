ca0c286edb71549195f1d2f22f8961cc87741b7e
shubham <shubham992284@gmail.com>
2024-04-07 11:00:57 +0530

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,6 +1,18 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const dotenv=require('dotenv');
+dotenv.config();
 
 const app=express();
+//connection to mongodb database
+mongoose.connect(process.env.MONGO).then(()=>
+{
+    console.log('connected to db');
+}
+).catch((err)=>{
+console.log('error');
+});
+
 
 app.listen(4000,(err)=>{
     console.log('server runing on port:4000');
