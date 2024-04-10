adfec8c5b7715b2e01d4d349e82006f3fcfcd223
shubham <shubham992284@gmail.com>
2024-04-10 14:20:38 +0530

    create signin api route

--- api/controller/auth.controller.js
+++ api/controller/auth.controller.js
@@ -1,6 +1,7 @@
 const User=require('../models/user.model.js');
 const {errorHandler}=require('../utils/error.js');
 const bcryptjs=require('bcryptjs');
+const jwt=require('jsonwebtoken');
 module.exports.signup=async(req,res,next)=>{
    const {username,email,password}=req.body;
    const hasedpassword=bcryptjs.hashSync(password,10);
@@ -13,4 +14,20 @@ module.exports.signup=async(req,res,next)=>{
    } catch (error) {
     next(error);
    } 
+}
+
+module.exports.signin=async(req,res,next)=>{
+   const {email,password}=req.body;
+   try {
+      const validUser=await User.findOne({email});
+   if(!validUser){return next(errorHandler('404',"user not founcd"))}
+   const validPassword=bcryptjs.compareSync(password, validUser.password);
+   if(!validPassword){return next(errorHandler('401',"wrong id or password"))};
+   const token=jwt.sign({id:validUser._id},process.env.JWT_SECRET)
+   const {password:pass,...rest}=validUser._doc
+   res.cookie('access_tokan',token,{httpOnly:true}).status(200).json(rest);
+   } catch (error) {
+      next(error);
+   }
+   
 }
